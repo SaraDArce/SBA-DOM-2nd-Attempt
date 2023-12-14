79322ff66a1da2851096445f54824ad9c1229388
Sara D'Arce <saradarce@gmail.com>
2023-12-14 02:57:19 -0500

    Setup and first func

---
+++ script.js
@@ -0,0 +1,8 @@
+// Function to create a new ToDo
+function createTodo() {
+  const userInput = {
+    task: document.getElementById("taskInput").value,
+    subtask: document.getElementById("subTaskInput").value,
+    notes: document.getElementById("notesInput").value,
+  };
+}
